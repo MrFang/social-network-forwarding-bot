ff392cefd3cb0e42e2ce7d62c702f6e0cef7b103
atera610 <atera.610@gmail.com>
2021-11-29 21:15:25 +0300

    added vk library, repost to vk

--- src/python/app.py
+++ src/python/app.py
@@ -2,6 +2,7 @@ import psycopg2
 import telebot
 import argparse
 import requests
+import vk
 
 parser = argparse.ArgumentParser(description='Hello DB web application')
 parser.add_argument(
@@ -23,11 +24,40 @@ connection = psycopg2.connect(
 )
 
 bot = telebot.TeleBot(args.tg_token, parse_mode=None)
-access_token = 'ad4d25a8ac7c44f965775921500543db4172658217066e97abd6571aa0802fc78d087d300d996cee8443d'
+vk_token = 'e0bbc661300067cbac41606ea905c2f49aa7db5d5e0e14f34fa31207283c96e38314d9dc59f2e07b17341'
+app_id = 8013095
+
+
+def init_session():
+    session = vk.Session(access_token=vk_token)
+    vk_api = vk.API(session,  v='5.131')
+    return vk_api
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    requests.post(f'https://api.vk.com/method/wall.post?v=5.131?owner_id=89685577&message={message.text}&access_token={access_token}')
+    # bot.reply_to(message, message.text)
+    vk_api = init_session()
+    vk_api.wall.post(message=message.text)
+
+
+@bot.message_handler(content_types=["photo"])
+def echo_all(message):
+    file_id = message.photo[-1].file_id
+    file_path = bot.get_file(file_id)
+    download_link = f'https://api.telegram.org/file/bot{args.tg_token}/{file_path}'
+
+    requests.get(download_link)
+    vk_api = init_session()
+    vk_photo_server = vk_api.photos.getWallUploadServer()
+    upload_url = vk_photo_server['upload_url']
+    resp = requests.post(upload_url, files={'file': open(file_path, 'rb')}).json()
+    photo = vk_api.photos.saveWallPhoto(server=resp['server'], photo=resp['photo'], hash=resp['hash'])
+    bot.file_path
+    bot.reply_to(message, file_path)
+    # link = f'https://api.telegram.org/file/bot{args.tg_token}/{path}'
+    # bot.send_photo(message.chat.id, file_id, caption=message.caption)
+    # requests.post(f'https://api.vk.com/method/wall.post?v=5.131?owner_id=89685577&message={message.caption}&attachments=photo{}&access_token={access_token}')
 
 
 bot.infinity_polling()
