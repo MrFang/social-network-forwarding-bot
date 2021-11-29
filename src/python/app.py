d50f253e6ac7308885009591b93dae07f00964dd
MrFang <bat312@yandex.ru>
2021-11-29 18:04:18 +0300

    Move from CLI arguments to dotenv

--- src/python/app.py
+++ src/python/app.py
@@ -1,33 +1,33 @@
 import psycopg2
 import telebot
-import argparse
 import requests
+from dotenv import dotenv_values
+import os
 
-parser = argparse.ArgumentParser(description='Hello DB web application')
-parser.add_argument(
-    '--pg-host', help='PostgreSQL host name', default='localhost')
-parser.add_argument('--pg-port', help='PostgreSQL port', default=5432)
-parser.add_argument('--pg-user', help='PostgreSQL user', default='postgres')
-parser.add_argument('--pg-password', help='PostgreSQL password', default='')
-parser.add_argument(
-    '--pg-database', help='PostgreSQL database', default='postgres')
-parser.add_argument('--tg-token', help='Telegram bot token', default='')
+if not os.path.isfile('.env'):
+    raise RuntimeError('.env file does not exist')
 
-args = parser.parse_args()
+config = dotenv_values('.env')
 
 connection = psycopg2.connect(
-    database=args.pg_database,
-    user=args.pg_user,
-    host=args.pg_host,
-    password=args.pg_password
+    database='postgres',
+    user='postgres',
+    host=config['SNF_BOT_DB_HOST'],
+    password=config['SNF_BOT_DB_PASS']
 )
 
-bot = telebot.TeleBot(args.tg_token, parse_mode=None)
-access_token = 'ad4d25a8ac7c44f965775921500543db4172658217066e97abd6571aa0802fc78d087d300d996cee8443d'
+bot = telebot.TeleBot(config['SNF_BOT_TELEGRAM_TOKEN'], parse_mode=None)
+access_token = config['SNF_BOT_VK_TOKEN']  # TODO: move to DB
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    requests.post(f'https://api.vk.com/method/wall.post?v=5.131?owner_id=89685577&message={message.text}&access_token={access_token}')
+    requests.post('https://api.vk.com/method/wall.post?'
+                  'v=5.131&'
+                  'owner_id=89685577&'  # TODO move to DB
+                  f'message={message.text}&'
+                  f'access_token={access_token}'
+                  )
 
 
 bot.infinity_polling()
