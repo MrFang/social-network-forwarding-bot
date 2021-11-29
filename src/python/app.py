597fda81ecd62c5ab58b48d8335207f0adcb1ea5
atera610 <atera.610@gmail.com>
2021-11-29 17:11:03 +0300

    echo to vk page on test user (#1)

--- src/python/app.py
+++ src/python/app.py
@@ -1,6 +1,7 @@
 import psycopg2
 import telebot
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description='Hello DB web application')
 parser.add_argument(
@@ -22,11 +23,11 @@ connection = psycopg2.connect(
 )
 
 bot = telebot.TeleBot(args.tg_token, parse_mode=None)
-
+access_token = 'ad4d25a8ac7c44f965775921500543db4172658217066e97abd6571aa0802fc78d087d300d996cee8443d'
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    requests.post(f'https://api.vk.com/method/wall.post?v=5.131?owner_id=89685577&message={message.text}&access_token={access_token}')
 
 
 bot.infinity_polling()
