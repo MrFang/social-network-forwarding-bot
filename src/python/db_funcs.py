64d9ef9e7eb9eac9e8817885fe4926a59e5c91f6
MrFang <bat312@yandex.ru>
2021-11-30 20:13:42 +0300

    Add correct register link

--- src/python/db_funcs.py
+++ src/python/db_funcs.py
@@ -7,10 +7,13 @@ def get_all_connections(cur, user_id):
     if len(data) == 0:
         return False
     for num, link in enumerate(data):
-        message += f"{num+1}) {link['channel_id']} - {link['vk_access_token']} \n"
+        message += f"{num+1}) " \
+            "{link['channel_id']} - {link['vk_access_token']}\n"
     return message
 
+
 def add_new_line(cur, channel_id, vk_access_token, user_id):
-    cur.execute("INSERT INTO channel_to_vk (channel_id, vk_access_token, issued_by) VALUES (%s, %s, %s)",
-                (channel_id, vk_access_token, user_id)
-                )
+    cur.execute('''
+        INSERT INTO channel_to_vk
+        (channel_id, vk_access_token, issued_by) VALUES
+        (%s, %s, %s)''', (channel_id, vk_access_token, user_id))
