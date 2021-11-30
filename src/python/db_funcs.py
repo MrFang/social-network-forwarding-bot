3a4a177749979a75d90059f2148d867da171a663
KoverevEvgeniy <koverev.evg@gmail.com>
2021-11-30 17:36:15 +0300

    dict changet to db partly

---
+++ src/python/db_funcs.py
@@ -0,0 +1,16 @@
+def get_all_connections(cur, user_id):
+    cur.execute("SELECT * FROM channel_to_vk WHERE issued_by = %s",
+                (user_id, )
+                )
+    data = cur.fetchall()
+    message = "Your active links are: \n"
+    if len(data) == 0:
+        return False
+    for num, link in enumerate(data):
+        message += f"{num+1}) {link['channel_id']} - {link['vk_access_token']} \n"
+    return message
+
+def add_new_line(cur, channel_id, vk_access_token, user_id):
+    cur.execute("INSERT INTO channel_to_vk (channel_id, vk_access_token, issued_by) VALUES (%s, %s, %s)",
+                (channel_id, vk_access_token, user_id)
+                )
